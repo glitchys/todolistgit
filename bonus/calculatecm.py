7defbf38675875c4255b0d710906783bcf2ad4df
Yousif Omar <76589534+glitchys@users.noreply.github.com>
2026-02-01 11:36:55 +0300

    Created a time-stamp and updated import statements

--- bonus/calculatecm.py
+++ bonus/calculatecm.py
@@ -1,14 +1,12 @@
+from bonus.converters import convert
+from bonus.parsers import parse
+
 feet_inches = input("Enter feet and inches: ")
 
+parsed = parse(feet_inches)
+result = convert(parsed['feet'], parsed['inches'])
+print(f"{parsed['feet']} feet and {parsed['inches']} inches is {result} meters.")
 
-def convert(feet_inches):
-   parts = feet_inches.split(" ")
-   feet = float(parts[0])
-   inches = float(parts[1])
-   meter = feet * 30.48 + inches * 2.54
-   return meter
-
-result = convert(feet_inches)
 
 if result < 1:
     print("Too small.")
